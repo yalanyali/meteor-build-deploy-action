e2afb94d6d628777f40f00729e9dbdbd471f9435
Your Name <you@example.com>
2020-10-12 16:13:48 +0200

    foo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-echo "SSH_KEY TEST"
-echo ${SSH_KEY} # *****
-
-echo "KNOWN_HOSTS TEST"
-echo ${KNOWN_HOSTS} # ****
-
-exit
-
 # install curl
 apt-get update && apt install curl -y
-
 
 # install meteor
 curl https://install.meteor.com/ | sh
@@ -22,7 +13,7 @@ npm install --global mup
 meteor npm install --production
 
 # Create pem
-mkdir /home/yigit && mkdir /home/yigit/.ssh
-echo ${ID_RSA} > /home/yigit/.ssh/id_rsa
-chmod 600 /home/yigit/.ssh/id_rsa
+mkdir -p ~/.ssh
+echo ${SSH_KEY} > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
 cd .deploy && mup stop && mup setup && mup deploy --settings ./settings.json
