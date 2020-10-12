9bad60ac1e8adbccd59e2e739402daddd9ec0d23
Your Name <you@example.com>
2020-10-12 15:29:32 +0200

    foo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+echo "ID_RSA TEST"
+echo $id_rsa # *****
+if [ -z "$id_rsa" ]
+then
+      echo "\$var is empty"
+else
+      echo "\$var is NOT empty"
+fi
+
 # install curl
-apt-get update && apt install -y curl
+apt-get update && apt install curl -y
+
 
 # install meteor
 curl https://install.meteor.com/ | sh
@@ -12,10 +22,8 @@ npm install --global mup
 # create build
 meteor npm install --production
 
-# create pem
+# Create pem
 mkdir /home/yigit && mkdir /home/yigit/.ssh
 echo $ID_RSA > /home/yigit/.ssh/id_rsa
 chmod 600 /home/yigit/.ssh/id_rsa
-
-# deploy
 cd .deploy && mup stop && mup setup && mup deploy --settings ./settings.json
