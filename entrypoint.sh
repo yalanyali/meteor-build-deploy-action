a8af13f62aaff8f99b260fb3b385dd33d6496580
Your Name <you@example.com>
2020-10-12 15:15:06 +0200

    id_rsa

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,10 @@ npm install --global mup
 
 # create build
 meteor npm install --production
+
+# create pem
+echo $ID_RSA > /home/yigit/.ssh/id_rsa
+chmod 600 /home/yigit/.ssh/id_rsa
+
+# deploy
 cd .deploy && mup stop && mup setup && mup deploy --settings ./settings.json
